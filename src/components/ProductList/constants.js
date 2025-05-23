ff8ee081416d384121ca78613ad6a553d94ebc8a
Ritesh Kumar Panda <ritesh.ku.panda.925@gmail.com>
2025-05-23 11:09:15 +0530

    Added queryParams

---
+++ src/components/ProductList/constants.js
@@ -0,0 +1,3 @@
+export const DEFAULT_PAGE_SIZE = 4;
+
+export const DEFAULT_PAGE_INDEX = 1;
--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -2,21 +2,51 @@ import { useState } from "react";
 
 import { Header, PageLoader } from "components/commons";
 import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
-import useDebounce from "hooks/useDebounce";
+import useFuncDebounce from "hooks/useFuncDebounce";
+import useQueryParams from "hooks/useQueryParams";
+import { filterNonNull } from "neetocist";
 import { Search } from "neetoicons";
-import { Input, NoData } from "neetoui";
-import { isEmpty } from "ramda";
+import { Input, NoData, Pagination } from "neetoui";
+import { isEmpty, mergeLeft } from "ramda";
+import { useHistory } from "react-router-dom";
+import routes from "routes";
+import { buildUrl } from "utils/url";
 
+import { DEFAULT_PAGE_SIZE, DEFAULT_PAGE_INDEX } from "./constants";
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
-  const [searchKey, setSearchKey] = useState("");
+  const queryParams = useQueryParams();
+  const { page, pageSize, searchTerm = "" } = queryParams;
 
-  const debouncedSearchKey = useDebounce(searchKey);
+  const [searchKey, setSearchKey] = useState(searchTerm);
 
-  const { data: { products = [] } = {}, isLoading } = useFetchProducts({
-    searchTerm: debouncedSearchKey,
+  const history = useHistory();
+  const handlePageNavigation = page =>
+    history.replace(
+      buildUrl(
+        routes.products.index,
+        mergeLeft({ page, pageSize: DEFAULT_PAGE_SIZE }, queryParams)
+      )
+    );
+
+  const updateQueryParams = useFuncDebounce(value => {
+    const params = {
+      page: DEFAULT_PAGE_INDEX,
+      pageSize: DEFAULT_PAGE_SIZE,
+      searchTerm: value || null,
+    };
+    setSearchKey(value);
+
+    history.replace(buildUrl(routes.products.index, filterNonNull(params)));
   });
+
+  const { data: { products = [], totalProductsCount } = {}, isLoading } =
+    useFetchProducts({
+      searchTerm,
+      page: Number(page) || DEFAULT_PAGE_INDEX,
+      pageSize: Number(pageSize) || DEFAULT_PAGE_SIZE,
+    });
 
   if (isLoading) {
     return <PageLoader />;
@@ -33,7 +63,10 @@ const ProductList = () => {
             prefix={<Search />}
             type="search"
             value={searchKey}
-            onChange={event => setSearchKey(event.target.value)}
+            onChange={({ target: { value } }) => {
+              updateQueryParams(value);
+              setSearchKey(value);
+            }}
           />
         }
       />
@@ -56,6 +89,15 @@ const ProductList = () => {
           ))}
         </div>
       )}
+      <div className="mb-5 self-end">
+        <Pagination
+          className=""
+          count={totalProductsCount}
+          navigate={handlePageNavigation}
+          pageNo={Number(page) || DEFAULT_PAGE_INDEX}
+          pageSize={Number(pageSize) || DEFAULT_PAGE_SIZE}
+        />
+      </div>
     </div>
   );
 };
